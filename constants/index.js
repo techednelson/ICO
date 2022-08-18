885147da42890356b1327de3a557739b066d9e33
Nelson Sandoval <techednelson@gmail.com>
2022-08-18 10:06:54 +0300

    initial commit

---
+++ constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x91ea267Dd0f4C523C580B309E2413c62AffDE05D";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
